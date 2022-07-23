6b8fffc5fe78509975e148d6c20179473d9b2e51
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-07-23 13:11:27 +0800

    update like_button.js

--- 02-frontend-with-react/like_button.js
+++ 02-frontend-with-react/like_button.js
@@ -20,3 +20,5 @@ class LikeButton extends React.Component {
     );
   }
 }
+
+// This code defines a React component called LikeButton. Don’t worry if you don’t understand it yet — we’ll cover the building blocks of React later in our hands-on tutorial and main concepts guide. For now, let’s just get it showing on the screen!
