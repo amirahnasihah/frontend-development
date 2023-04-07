3fecfee8b91fe97744f93cafd4416f3a3ea31354
amirahnasihah <amirahnasihah97@gmail.com>
2023-04-07 21:24:29 +0800

    lab 1 algo

---
+++ 04-intro-to-algorithms/01-introduction/exercise-1.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+// Task: complete the function that takes in an input array and a search target
+// If the target exists in inputArray, then the function should return true
+// If the target does not exist in inputArray, then the function should return false
+const find = (inputArray, target) => {
+  // Put your code below
+};
+
+// DO NOT MODIFY THE CODE BELOW
+// Test Cases
+assert.strictEqual(find([1, 3, 5, 7, 9], 5), true);
+assert.strictEqual(find([1, 3, 5, 7, 9], 0), false);
+assert.strictEqual(find([1, 3, 5, 7, 9], 3), true);
+console.log("All Tests Passed.");
---
+++ 04-intro-to-algorithms/01-introduction/exercise-2.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+
+// Task: complete the function that takes in a number and return the value of its factorial
+// Factorial of 5 = 5! = 5 * 4 * 3 * 2 * 1 = 120
+// Note that 0! = 1, and 1! = 1
+
+const factorial = (input) => {
+  // Put your code below
+};
+
+// DO NOT MODIFY THE CODE BELOW
+// Test Cases
+assert.strictEqual(factorial(1), 1);
+assert.strictEqual(factorial(0), 1);
+assert.strictEqual(factorial(5), 120);
+assert.strictEqual(factorial(3), 6);
+console.log("All Tests Passed.");
