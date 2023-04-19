ddf393910909ad35dfd3a211ba57595b45395722
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-04-19 12:44:54 +0800

    update 04-intro-to-algorithms/05-sorting-algorithms-part-01/insertion-sort.js

--- 04-intro-to-algorithms/05-sorting-algorithms-part-01/insertion-sort.js
+++ 04-intro-to-algorithms/05-sorting-algorithms-part-01/insertion-sort.js
@@ -4,14 +4,24 @@ const assert = require('assert');
 // The function should return the sorted array
 // You should use insertion sort algorithm
 
-const insertionSort = (arr) => {   
+const insertionSort = (arr) => {
   // Copy the input array and work on the cloned array
   // In JS, we always try to work in an immutable way
   let inputArr = [...arr]
 
   // Main Logic
   // Add your code here
+  for(let i = 1; i < inputArr.length; i++) {
+    let current = inputArr[i];
+    let j = i - 1;
 
+    while(j >= 0 && current < inputArr[j]) {
+      inputArr[j + 1] = inputArr[j];
+      j--;
+    }
+    inputArr[j + 1] = current;
+  }
+  return inputArr;
 }
 
 // DO NOT MODIFY CODE BELOW
