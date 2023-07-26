b5333126368dc6e80860a975c3bb7531afa29300
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-07-26 16:03:32 +0800

    update brute-force-test.js

--- 04-intro-to-algorithms/01-introduction/brute-force-test.js
+++ 04-intro-to-algorithms/01-introduction/brute-force-test.js
@@ -1,9 +1,9 @@
 inputArray = [5, 3, 7, 2, 5, 9, 0, 3];
 
-tempMaxNumber = 2;
+tempMaxNumber = 0;
 
 for (i of inputArray) {
-	if (tempMaxNumber === 2) {
+	if (tempMaxNumber === 0) {
 		tempMaxNumber = i;
 	} else {
 		if (i > tempMaxNumber) {
@@ -14,3 +14,10 @@ for (i of inputArray) {
 
 console.log(tempMaxNumber);
 // 9
+
+If you change `null` to a number like `4` in the `tempMaxNumber` initialization, it will affect how the code finds the maximum number in the `inputArray`. 
+With `tempMaxNumber = 4`, the code will still correctly find the maximum number in the `inputArray`, but the initial value of `tempMaxNumber` becomes `4`. Therefore, the algorithm will always find a number in the array greater than or equal to `4`.
+
+In the given example, the maximum number in the `inputArray` is `9`. So, even though `tempMaxNumber` starts with the value `4`, it will be correctly updated to `9` after iterating through the `inputArray`, and the output of `console.log(tempMaxNumber)` will be `9`.
+
+However, if you had set `tempMaxNumber` to a number smaller than the maximum number in the array (e.g., `tempMaxNumber = 2`), the algorithm would not find the correct maximum value. The initial value would limit the algorithm's ability to identify the maximum number in the array accurately. Therefore, when using an initial value other than `null`, make sure it is greater than or equal to any possible value in the array to ensure correctness.
