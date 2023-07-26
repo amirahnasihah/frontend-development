a66844e91edaa16d4f2e2e7712e38be4484011c2
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-07-26 15:59:40 +0800

    update brute-force-test.js

--- 04-intro-to-algorithms/01-introduction/brute-force-test.js
+++ 04-intro-to-algorithms/01-introduction/brute-force-test.js
@@ -3,14 +3,13 @@ inputArray = [5, 3, 7, 2, 5, 9, 0, 3];
 tempMaxNumber = null;
 
 for (i of inputArray) {
-  if (tempMaxNumber === null) {
-    tempMaxNumber = i;
-  }
-  else {
-    if (i > tempMaxNumber) {
-      tempMaxNumber = i;
-    }
-  }
+	if (tempMaxNumber === null) {
+		tempMaxNumber = i;
+	} else {
+		if (i > tempMaxNumber) {
+			tempMaxNumber = i;
+		}
+	}
 }
 
 console.log(tempMaxNumber)
