aaedde09b5411c21175d8a1e5cc1dbf7c6e372ef
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-06 09:10:09 +0800

    update test.js

---
+++ 04-intro-to-algorithms/02-array-algorithms/test.js
@@ -0,0 +1,5 @@
+fruits = ['banana', 'guava', 'apple', 'apple', 'apple', 'orange', 'grapes', 'grapes']
+
+fruits = ['banana', 'guava', 'apple', 'apple', 'apple', 'orange', 'grapes', 'grapes']
+
+console.log(fruits.indexOf("apple"))
