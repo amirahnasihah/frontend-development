9cef257567fd9faba0ca129fb50f427b19589346
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-06 09:13:15 +0800

    update test.js

--- 04-intro-to-algorithms/02-array-algorithms/test.js
+++ 04-intro-to-algorithms/02-array-algorithms/test.js
@@ -1,4 +1,5 @@
 fruits = ['banana', 'guava', 'apple', 'apple', 'apple', 'orange', 'grapes', 'grapes']
 
 console.log(fruits.indexOf("apple")) // 2
-console.log(fruits[5])
+console.log(fruits.indexOf("mangosteen")) // -1, not exist
+console.log(fruits[5]) // orange
