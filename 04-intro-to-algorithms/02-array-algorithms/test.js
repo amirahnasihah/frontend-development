8f3baff57af1d5492292d735d9dd9beca30a19e6
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-06 09:11:13 +0800

    update test.js

--- 04-intro-to-algorithms/02-array-algorithms/test.js
+++ 04-intro-to-algorithms/02-array-algorithms/test.js
@@ -2,4 +2,4 @@ fruits = ['banana', 'guava', 'apple', 'apple', 'apple', 'orange', 'grapes', 'gra
 
 fruitsLong = ['banana', 'guava', 'red apple', 'apple', 'apple', 'orange', 'grapes purple', 'grapes']
 
-console.log(fruitsLong.indexOf("apple"))
+console.log(fruitsLong.indexOf("grapes"))
