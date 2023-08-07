562e1a1c374b7f2f204918b306072d00ec4abc60
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-08 07:05:40 +0800

    update test.js

--- 04-intro-to-algorithms/03-string-algorithms/test.js
+++ 04-intro-to-algorithms/03-string-algorithms/test.js
@@ -9,17 +9,16 @@ console.log("Array String:", arrStr[6]) // Print out the letter “W”
 // Strings Question 1 - Finding Substring: How can you check if a substring exists in a string? (How to implement the indexOf() function?)
 // why use indexOf()?
 
-my answer:
 originalString = "Hello"
 target = "lo"
 
 // Part 1 Create Outer Loop - h,e,l,l,o
 for (let i = 0; i < originalString.length; i++) {
-      // Inner Loop - must be unique; e.l.l
-      for (let j = i + 1; j < originalString.length; j++) {
-            if (originalString[i] + originalString[j] === target) {
-             console.log("targetStr:", originalString[i] + "," + originalString[j]
-           }
-        }
+	// Inner Loop - must be unique; e.l.l
+	for (let j = i + 1; j < originalString.length; j++) {
+		if (originalString[i] + originalString[j] === target) {
+			console.log("targetStr:", originalString[i] + "," + originalString[j])
+		}
+	}
 
 }
