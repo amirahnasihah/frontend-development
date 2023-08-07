21e17b103f42a4935547a4507eca55ff96dca6e3
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-08 03:10:01 +0800

    update test.js

--- 04-intro-to-algorithms/02-array-algorithms/test.js
+++ 04-intro-to-algorithms/02-array-algorithms/test.js
@@ -65,7 +65,7 @@ for (let i = 0; i < list.length; i++) {
 	for (let j = i + 1; list.length; j++) {
 		if (list[i] + list[j] === target) {
 			console.log("Answer:", list[i] + "," + list[j])
-			twoNum.push(list[i])
+			// twoNum.push(list[i], list[j])
 		}
 	}
 }
