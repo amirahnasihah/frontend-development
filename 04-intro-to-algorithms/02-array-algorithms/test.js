3613fa71edfdb33430567a23202f31098de6b6dc
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-08 03:08:45 +0800

    update test.js

--- 04-intro-to-algorithms/02-array-algorithms/test.js
+++ 04-intro-to-algorithms/02-array-algorithms/test.js
@@ -58,13 +58,14 @@ console.log(pairs)
 list = [1721, 979, 366, 299, 756, 1456]
 target = 2020
 
-let su
+let twoNum = []
 
 for (let i = 0; i < list.length; i++) {
 	// second loop + avoid duplicate output (unique result)
 	for (let j = i + 1; list.length; j++) {
 		if (list[i] + list[j] === target) {
 			console.log("Answer:", list[i] + "," + list[j])
+			twoNum.push(list[i])
 		}
 	}
 }
