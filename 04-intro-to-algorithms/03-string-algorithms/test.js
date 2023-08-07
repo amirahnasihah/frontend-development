14103225ea1aeef22dff596277e845b9246c5fdb
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-08 03:55:48 +0800

    update test.js

--- 04-intro-to-algorithms/03-string-algorithms/test.js
+++ 04-intro-to-algorithms/03-string-algorithms/test.js
@@ -1,5 +1,5 @@
 const str = "Hello World!"
-const arrStr = [“H”, “e”, “l”, "l", "o", "W", "o", "r", "l", "d"]
+const arrStr = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"]
 
 console.log("String:", str[1]) // Print out the letter “e”
 console.log("Array String:", arrStr[6]) // Print out the letter “W”
