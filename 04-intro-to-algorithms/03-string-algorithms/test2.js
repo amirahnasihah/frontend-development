70e200680be9d8c041752afcc5fddfefea8b5bd6
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-10 01:57:08 +0800

    update test2.js

--- 04-intro-to-algorithms/03-string-algorithms/test2.js
+++ 04-intro-to-algorithms/03-string-algorithms/test2.js
@@ -12,3 +12,22 @@ for (let i = inputString.length - 1; i >= 0; i--) {
 }
 
 console.log(result); // sbaltnelaT
+
+
+// TalentLabs Solution
+originalString = "Hello";
+target = "lo";
+
+for (let i = 0; i < originalString.length; i++) {
+  // Leave for Step 2
+  for (let j = 0; j < targetString.length; j++) {
+    // Case 1: Not Match, and end this iteration
+    if (originalString[i + j] !== targetString[j]) {
+      break;
+    }
+    // Case 2: Match and it’s the last character
+    else if (j === targetString.length - 1) {
+      console.log("Found at position " + i);
+    }
+  }
+}
