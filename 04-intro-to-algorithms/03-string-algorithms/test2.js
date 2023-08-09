15c550a00e61450ead47229d12ee2ec5ca32ddc6
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-10 02:26:08 +0800

    update test2.js

--- 04-intro-to-algorithms/03-string-algorithms/test2.js
+++ 04-intro-to-algorithms/03-string-algorithms/test2.js
@@ -1,4 +1,27 @@
 /*
+Strings Question 1 - Finding Substring: How can you check if a substring exists in a string? (How to implement the indexOf() function?)
+
+*/
+
+originalString = "Hello";
+targetString = "lo";
+
+// TalentLabs Solution - Optimization
+for (let i = 0; i < originalString.length; i++) {
+  // Leave for Step 2
+  for (let j = 0; j < targetString.length; j++) {
+    // Case 1: Not Match, and end this iteration
+    if (originalString[i + j] !== targetString[j]) {
+      break;
+    }
+    // Case 2: Match and it’s the last character
+    else if (j === targetString.length - 1) { // optimize
+      console.log("Found at position " + i);
+    }
+  }
+}
+
+/*
 Strings Question 2 - Reverse Word Order
 
 Simple solution: loop start at last index and continue until reach the first index, bcsits reverse
@@ -20,25 +43,6 @@ const inputString = "Talentlabs"
 const reversedString = inputString.split("").reverse().join("")
 
 console.log(reversedString)
-
-originalString = "Hello";
-targetString = "lo";
-
-// TalentLabs Solution - Optimization
-for (let i = 0; i < originalString.length; i++) {
-  // Leave for Step 2
-  for (let j = 0; j < targetString.length; j++) {
-    // Case 1: Not Match, and end this iteration
-    if (originalString[i + j] !== targetString[j]) {
-      break;
-    }
-    // Case 2: Match and it’s the last character
-    else if (j === targetString.length - 1) { // optimize
-      console.log("Found at position " + i);
-    }
-  }
-}
-
 
 /*
 Anagram
