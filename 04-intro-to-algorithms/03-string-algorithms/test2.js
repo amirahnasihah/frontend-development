4a3ec47a97bae8ef2ef7b84dcac45bbf499eb6dc
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-10 02:11:26 +0800

    update test2.js

--- 04-intro-to-algorithms/03-string-algorithms/test2.js
+++ 04-intro-to-algorithms/03-string-algorithms/test2.js
@@ -30,3 +30,27 @@ for (let i = 0; i < originalString.length; i++) {
     }
   }
 }
+
+
+/*
+Anagram
+
+*/
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words[4]);
+// Expected output: "jumps"
+
+const chars = str.split('');
+console.log(chars[8]);
+// Expected output: "k"
+
+const letter = str.split('');
+console.log(letter);
+// Expected output: Array ["T", "h", "e", " ", "q", "u", "i", "c", "k", " ", "b", "r", "o", "w", "n", " ", "f", "o", "x", " ", "j", "u", "m", "p", "s", " ", "o", "v", "e", "r", " ", "t", "h", "e", " ", "l", "a", "z", "y", " ", "d", "o", "g", "."]
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
