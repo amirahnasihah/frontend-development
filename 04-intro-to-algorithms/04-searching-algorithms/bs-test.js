6456bdda0e97d92782e683209014417d2fe9cd54
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-15 03:08:27 +0800

    update bs-test.js

---
+++ 04-intro-to-algorithms/04-searching-algorithms/bs-test.js
@@ -0,0 +1,28 @@
+// while loop
+
+const arr = [1, 2, 4, 6, 7, 9, 10, 11, 13];
+const target = 7
+
+// Control the search range
+let start = 0
+let end = arr.length - 1
+
+// Exit Condition: start is larger than the end
+while (start <= end) {
+    // Handle the odd length scenario
+    let middle = Math.floor((start + end) / 2) 
+    
+    // Found!
+    if (arr[middle] === target) {
+        console.log("Found at position: " + middle);
+        break;
+    }
+    else if (arr[middle] < target) {
+        start = middle + 1;
+    }
+    else {
+        end = middle - 1;
+    }
+}
+
+// output: 
