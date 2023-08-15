46a33b51f16f22dc6b2cf9596a76faf2ca0d13b4
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-15 09:08:17 +0800

    update 04-intro-to-algorithms/03-string-algorithms/test.js

--- 04-intro-to-algorithms/03-string-algorithms/test.js
+++ 04-intro-to-algorithms/03-string-algorithms/test.js
@@ -1,7 +1,7 @@
 const str = "Hello World!";
 const arrStr = ["H", "e", "l", "l", "o", " ", "W", "o", "r", "l", "d"];
 
-// array[index]
+// array[index] will show element value
 console.log("String:", str[1]); // Print out the letter “e”
 console.log("String space:", str[5]);
 console.log("Array String:", arrStr[6]); // Print out the letter “W”
