20ddeacd89b85cb776912bd0f8fafe8eb0cd42d4
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-16 14:27:12 +0800

    update test.js

---
+++ 04-intro-to-algorithms/05-sorting-algorithms-part-01/test.js
@@ -0,0 +1,15 @@
+/*
+For more complex sorting, you can customize the comparison function. If you need to sort objects based on a specific property, like sorting an array of objects by their ages:
+
+*/
+
+const people = [
+  { name: "Alice", age: 30 },
+  { name: "Bob", age: 25 },
+  { name: "Eve", age: 35 },
+];
+
+people.sort((a, b) => a.age - b.age);
+console.log(people);
+
+// 
