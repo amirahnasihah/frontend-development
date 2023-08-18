bf63cff511ce36dc216bd37044c913a812fffb08
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-19 02:17:04 +0800

    update ms-test.js

--- 04-intro-to-algorithms/05-sorting-algorithms-part-02/ms-test.js
+++ 04-intro-to-algorithms/05-sorting-algorithms-part-02/ms-test.js
@@ -5,7 +5,7 @@ splice(startIndex, deleteCount, ...addTo) will create new array from fruits, and
 const fruits = ["cherry", "strawberry", "apple", "banana", "orange", "pear", "grape"]
 
 const half = Math.floor(fruits.length / 2)
-const spliced = fruits.splice(0, half) // output: cherry, strawberry, apple
+const spliced = fruits.splice(0, half)
 
-console.log("half:", half)
-console.
+console.log("half:", half) // output: 3
+console.log("splice:", spliced) // output: cherry, strawberry, apple
