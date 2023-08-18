19c0b36d08b3c815ebb342ce41095dc344fb8ab4
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-19 02:12:55 +0800

    update ms-test.js

--- 04-intro-to-algorithms/05-sorting-algorithms-part-02/ms-test.js
+++ 04-intro-to-algorithms/05-sorting-algorithms-part-02/ms-test.js
@@ -1,7 +1,7 @@
 /*
-splice() will create
+splice() will create new array from fruits, and fruits array will be modified
 */
 
 const fruits = ["cherry", "strawberry", "apple", "banana", "orange", "pear", "grape"]
 
-let
+let spliced = 
