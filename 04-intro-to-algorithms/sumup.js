3c88dd7433f27a2f9438b4d86fdef18015e4c813
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-24 02:32:38 +0800

    update sumup.js

--- 04-intro-to-algorithms/sumup.js
+++ 04-intro-to-algorithms/sumup.js
@@ -1,6 +1,7 @@
 /* Bubble Sort */
 function bubbleSort(bubbleFruits) {
-	// 
+	// compare neighboring elements from start to second last element
+	
 }
 
 const unsortBubbleFruits = ["banana", "strawberry","apple", "kiwi", "grape", "pear", "mango"];
