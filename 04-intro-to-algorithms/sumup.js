3195b9c02f07dca3768340140506b43f0f1bd2ca
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-24 02:30:16 +0800

    update sumup.js

--- 04-intro-to-algorithms/sumup.js
+++ 04-intro-to-algorithms/sumup.js
@@ -3,4 +3,6 @@ function bubbleSort(bubbleFruits) {
 	
 }
 
-const unsortBubbleFruits = ["banana", "strawberry","apple", "kiwi""grape", "pear", "mango"];
+const unsortBubbleFruits = ["banana", "strawberry","apple", "kiwi", "grape", "pear", "mango"];
+const sortBubbleFruits = unsortBubbleFruits
+bubbleFruits(sortBubbleFruits)
