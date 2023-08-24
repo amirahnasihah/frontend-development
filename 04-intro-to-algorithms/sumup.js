d6f753a8bd72c864426d85693360fab149d03303
Amirah Nasihah <amirahnasihah97@gmail.com>
2023-08-24 12:36:05 +0800

    update sumup.js

--- 04-intro-to-algorithms/sumup.js
+++ 04-intro-to-algorithms/sumup.js
@@ -20,7 +20,7 @@ function bubbleSort(bubbleFruits) {
 const unsortedNumb = [64, 34, 25, 12, 22, 11, 90];
 // const unsortBubbleFruits = ["banana", "strawberry","apple", "kiwi", "grape", "pear", "mango"];
 const sortBubbleFruits = bubbleSort(unsortedNumb)
-console.log(sortBubbleFruits) // output: [11, 12, 22, 25, 34, 64, 90]
+console.log("Bubble Sort:", sortBubbleFruits) // output: [11, 12, 22, 25, 34, 64, 90]
 
 /*
 OR SWAP:
@@ -29,3 +29,20 @@ let temp = bubbleFruits[j]
 bubbleFruits[j] = bubbleFruits[j + 1]
 bubbleFruits[j + 1] = temp
 */
+
+/* Insertion Sort */
+const arr = [12, 9, 20, 4, 5]
+
+for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && current < arr[j]) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = current;
+}
+
+console.log("Insertion Sort:", arr)
+// Output: [4, 5, 9, 12, 20]
